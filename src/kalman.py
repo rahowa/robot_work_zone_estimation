586f43a4088b18abca1e1640a1c19a588f1fa10f
rahowa <roninfromsvf@gmail.com>
2019-11-01 23:30:25 +0300

    change architecture of KalmanFilter and fix main() example

--- src/kalman.py
+++ src/kalman.py
@@ -7,26 +7,20 @@ from typing import List
 
 
 @dataclass
-class ErrorsEstimation:
+class Errors:
     x_coord: float 
     y_coord: float 
     v_x:     float 
     v_y:     float  
 
 
-@dataclass
-class ErrorsMeasurement:
-    x_coord: float 
-    y_coord: float 
-    v_x:     float 
-    v_y:     float 
-    
-
 class KalmanFilter(object):
-    def __init__(self, meas_errors: ErrorsMeasurement, est_errors: ErrorsEstimation):
+    def __init__(self, meas_errors: Errors, est_errors: Errors, 
+                 use_acceleration: bool):
         self.meas_errors = meas_errors
         self.est_errors = est_errors
         self.delta_t = 1.0
+        self.use_acceleration = use_acceleration
         self.transition_matrix_A = np.array([
             [1 ,0, self.delta_t, 0],
             [0, 1, 0, self.delta_t],
@@ -42,22 +36,23 @@ class KalmanFilter(object):
         self.speed_x = 0 
         self.speed_y = 0
         self.acceleration = 0
-        self.num_of_observations = 4
+        self.min_n_of_samples = 4
         self.covariance_est = self.covariance(est_errors.x_coord,
                                               est_errors.y_coord,
                                               est_errors.v_x,
                                               est_errors.v_y)
         self.history = deque(maxlen=100)
+        self.prev_state = np.zeros((4, 1))
 
     def update_speed(self, history: List[float]):
-        if len(history) > 1:
+        if len(history) > self.min_n_of_samples:
             history = np.array(self.history)
             speed = (history[1:, :] - history[:-1, :]).mean(0)
             self.speed_x = speed[0]
             self.speed_y = speed[1]
 
     def update_acceleration(self, history: List[float]):
-        if len(history) > 1:
+        if len(history) > self.min_n_of_samples:
             history = np.array(self.history)
             speed = (history[1:, :] - history[:-1, :])
             self.acceleration = (speed[1:, :] - speed[:-1, :]).mean()
@@ -72,23 +67,21 @@ class KalmanFilter(object):
         ])
         return np.diag(np.diag(cov_matrix))
 
-    def predict(self, x: int, y: int, v_x: int,
-                v_y: int, a:int) -> np.ndarray:
-        X = np.array([
-             [x],
-             [y],
-             [v_x],
-             [v_y]
-        ])
-        result = self.transition_matrix_A.dot(X)
-        result += self.transition_matrix_B.dot(a)
+    def predict(self, x: int, y: int, v_x: int, v_y: int) -> np.ndarray:
+        if len(self.history) >= self.min_n_of_samples:
+            X = np.array([
+                [x],
+                [y],
+                [v_x],
+                [v_y]
+            ])
+            result = self.transition_matrix_A.dot(X)
+            result += self.transition_matrix_B.dot(self.acceleration)
+        else:
+            result =  np.array([[x],[y],[v_x],[v_y]])
         return result
 
-    def update(self, new_x: int, new_y: int) -> np.ndarray:
-        self.history.append([new_x, new_y])
-        self.update_speed(self.history)
-        self.update_acceleration(self.history)
-        X = self.predict(new_x, new_y, self.speed_x, self.speed_y, self.acceleration)
+    def update(self, new_x: int, new_y: int, X_: np.ndarray) -> np.ndarray:
         self.covariance_est = np.diag(
             np.diag(
                 self.transition_matrix_A.dot(self.covariance_est).dot(self.transition_matrix_A.T)
@@ -110,39 +103,55 @@ class KalmanFilter(object):
         Y = H.dot(data).reshape(self.transition_matrix_A.shape[1], -1)
         # Update the State Matrix
         # Combination of the predicted state, measured values, covariance matrix and Kalman Gain
-        X = X + K.dot(Y - H.dot(X))
+        X_ = X_ + K.dot(Y - H.dot(X_))
         # Update Process Covariance Matrix
         self.covariance_est = (np.identity(len(K)) - K.dot(H)).dot(self.covariance_est)
-        return X
+        return X_
+
+    def step(self, new_x, new_y):
+        self.history.append([new_x, new_y])
+        self.update_speed(self.history)
+        X_pred = self.predict(self.prev_state[0][0], self.prev_state[1][0], 
+                              self.prev_state[2][0], self.prev_state[3][0])
+
+        if self.use_acceleration:
+            self.update_acceleration(self.history)
+
+        if len(self.history) < self.min_n_of_samples:
+            X_current =  np.array([[new_x], [new_y], [0], [0]])
+        else:
+            X_current = self.update(new_x, new_y, X_pred)
+
+        self.prev_state = X_current
+        return X_current
+
 
 def main():
     x_observations = np.arange(0, 1000, 10) + np.random.normal(0, 1, 100)
-    y_observations = np.sin(x_observations/10)+ np.random.normal(0, 1, 100)
+    y_observations = np.sin(np.arange(0, 1000, 10)/10)+ np.random.normal(0, 1, 100)
+    estimation_errors = Errors(10, 2, 1, 1)
+    observation_errors = Errors(10, 1, 1, 1)
+    x_filtered = [x_observations[0]]
+    y_filtered = [y_observations[0]]
 
-    # Process / Estimation Errors
-    # and
-    # Observation Errors
-    estimation_errors = ErrorsEstimation(20, 1, 5, 5)
-    observation_errors = ErrorsMeasurement(25, 1, 6, 6)
-    x_filtered = []
-    y_filtered = []
-
-    kf = KalmanFilter(observation_errors, estimation_errors)
-    for timestep in range(x_observations.shape[0]):
+    kf = KalmanFilter(observation_errors, estimation_errors, False)
+    for timestep in range(1, x_observations.shape[0]-1):
         new_x = x_observations[timestep]
         new_y = y_observations[timestep]
-
-        result = kf.update(new_x, new_y)
-        print(result, result.shape)
+        result = kf.step(new_x, new_y)
         x_filtered.append(result[0][0])
         y_filtered.append(result[1][0])
+        print(result, result.shape, kf.acceleration)
 
-    plt.plot(x_observations, y_observations, marker='o')
-    plt.plot(x_filtered, y_filtered, marker='x')
+
+    plt.style.use("ggplot")
+    plt.plot(np.arange(0, 1000, 10), np.sin(np.arange(0, 1000, 10)/10),
+             '--', label='original signal')
+    plt.plot(x_observations, y_observations, marker='o', 
+            label='noised signal')
+    plt.plot(x_filtered, y_filtered, marker='x', label='filtered signal')
+    plt.legend(loc='best')
     plt.show()
-
-
-
 
 
 if __name__ == "__main__":
