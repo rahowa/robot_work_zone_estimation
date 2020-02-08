e510e5b1805c294c8afd9db93bf9cb16a48ba62f
rahowa <roninfromsvf@gmail.com>
2020-02-08 18:07:13 +0600

    add data files

---
+++ src/calibrate_camera.py
@@ -0,0 +1,100 @@
+import os
+import cv2 
+import numpy as np 
+from glob import glob
+import typing as t
+from argparse import ArgumentParser
+
+
+CAM_WIDTH = 640
+CAM_HEIGHT = 480
+CHESSBOARD_WIDTH = 6
+CHESSBOARD_HEIGHT = 9
+IMG_SHAPE = (CAM_HEIGHT, CAM_WIDTH)
+CHESSBOARD_SHAPE = (CHESSBOARD_HEIGHT, CHESSBOARD_WIDTH)
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+
+def take_pictures(num_pictures: int, save_path: str) -> None: 
+    cap = cv2.VideoCapture(0)
+    cap.set(3, CAM_HEIGHT)
+    cap.set(4, CAM_WIDTH)
+    num_taken_pictures = 0
+    
+    while num_taken_pictures < num_pictures:
+        ret, frame = cap.read()
+        if not ret:
+            continue
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        corners_founded, corners = cv2.findChessboardCorners(gray, CHESSBOARD_SHAPE, None)
+        if corners_founded:
+            corners        = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            good_chess_img = cv2.drawChessboardCorners(gray, CHESSBOARD_SHAPE, corners, ret)
+            good_chess_img = cv2.putText(good_chess_img, "Good view",
+                                         (CAM_HEIGHT//2, CAM_WIDTH//2 - 30),
+                                         cv2.FONT_HERSHEY_SIMPLEX, 2,
+                                         (0, 255, 0))
+            cv2.imwrite(
+                os.path.join(save_path,
+                             f'frame_{num_taken_pictures}.jpg'),
+                frame
+            )        
+            num_taken_pictures += 1
+            cv2.imshow("frame", good_chess_img)
+        else:
+            cv2.imshow("frame", frame)
+
+        if cv2.waitKey(1) % 0xFF == ord('q'):
+            break
+        
+        # elif cv2.waitKey(60) == 32:
+            cv2.imwrite(
+                os.path.join(save_path,
+                             f'frame_{num_taken_pictures}.jpg'),
+                frame
+            )
+            num_taken_pictures += 1
+    
+    cv2.destroyAllWindows()
+    cap.release()
+
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--tp", action="store_true")
+    parser.add_argument("--path", default='./data/calibration', type=str)
+    parser.add_argument("--n", default=10, type=int)
+    args = parser.parse_args()
+
+    if args.tp:
+        take_pictures(args.n, args.path)
+
+    default_obj_points = np.zeros((CHESSBOARD_HEIGHT * CHESSBOARD_WIDTH, 3), np.float32)
+    default_obj_points[:, :2] = np.mgrid[0:CHESSBOARD_WIDTH, 0:CHESSBOARD_HEIGHT].T.reshape(-1, 2)
+
+    obj_points = []
+    img_points = []
+
+    images = glob(args.path + '/*.jpg')
+    for img_path in images:
+        img = cv2.imread(img_path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        ret, corners = cv2.findChessboardCorners(gray, CHESSBOARD_SHAPE, None)
+        if ret:
+            obj_points.append(default_obj_points)
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            img_points.append(corners)
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points,
+                                                       img_points,
+                                                       IMG_SHAPE,
+                                                       None, None)
+    print(mtx)
+
+
+# [10097  0       231]
+# [0      2495   312]
+# [0      0       1]
--- usage_exmaple.py
+++ usage_exmaple.py
@@ -13,20 +13,18 @@ from src.utills import (projection_matrix, render,
                     draw_corner)
 from src.kalman import KalmanFilter, Errors
 
+
 MIN_MATCHES = 15
 CAMERA_PARAMS = np.array([[800 , 0, 320], [0, 800, 240], [0, 0, 1]])
 
-
-def estimate_column_position() -> List[int]:
-    pass 
 
 def main():
     cap = cv2.VideoCapture(0)
     cap.set(3, 640)
     cap.set(4, 480)
 
-    obj_path = "./external/Cylinder.obj"
-    marker_path = "./external/test_column.jpg"
+    obj_path = "./src/data/3dmodels/Cylinder.obj"
+    marker_path = ".src./data/markers/test_column.jpg"
 
     obj = OBJ(obj_path, swapyz=True)
     scale_factor = 100.0
@@ -37,8 +35,6 @@ def main():
     column_descriptor = MakeDescriptor(cv2.ORB_create(**descriptor_params), 
                                        marker_path, 200, 200)
 
-    
-    homography_history = []
     estimation_errors = Errors(10, 2, 1, 1)
     observation_errors = Errors(10, 1, 1, 1)
     k_filter = KalmanFilter(observation_errors, estimation_errors, False)
@@ -51,8 +47,6 @@ def main():
             homography = homography_alg(kp_scene, kp_marker, des_scene, des_marker)
 
             if homography is not None:
-                # print(homography)
-                # exit()
                 scene = draw_corner(scene, column_descriptor.get_marker_size(), homography)
                 projection = projection_matrix(CAMERA_PARAMS, homography)
                 scene = render(scene, obj, scale_factor, 
@@ -66,7 +60,6 @@ def main():
                                         homography_alg.matches, 
                                         0, flags=2)
         cv2.imshow("kp", scene)
-        # exit()
         if cv2.waitKey(25) % 0xFF == ord('q'):
             break
 
