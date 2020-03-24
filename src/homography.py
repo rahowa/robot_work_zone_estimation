09c12d83276e340bb4cdfd812499c2348ec7b5c9
rahowa <roninfromsvf@gmail.com>
2020-03-24 23:59:08 +0300

    fix types

--- src/homography.py
+++ src/homography.py
@@ -1,6 +1,7 @@
 import math
 import cv2 
 import numpy as np
+from nptyping import Array
 from typing import Tuple, List, Sequence, Union, Any
 
 class ComputeHomography:
@@ -11,17 +12,17 @@ class ComputeHomography:
     def _find_homography(self,
                         kp_frame: List[cv2.KeyPoint], 
                         kp_marker: List[cv2.KeyPoint], 
-                        matches: List[cv2.DMatch]) -> np.ndarray:
+                        matches: List[cv2.DMatch]) -> Array[float]:
         src_pts = np.float32([kp_marker[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
         dst_pts = np.float32([kp_frame[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
-        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        M, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
         return M
 
     def __call__(self, 
                 kp_frame: List[cv2.KeyPoint], 
                 kp_marker: List[cv2.KeyPoint],
                 des_frame: List[cv2.DMatch],
-                des_marker: List[cv2.DMatch]) -> np.ndarray:
+                des_marker: List[cv2.DMatch]) -> Array[float]:
         
         if isinstance(self.matcher, (cv2.FlannBasedMatcher)):
             self.matches = self.matcher.knnMatch(des_marker, des_frame, k=2)
