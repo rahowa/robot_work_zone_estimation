a081bcca850b74b9776cc0703f878a6a3e00526c
rahowa <roninfromsvf@gmail.com>
2020-04-26 21:38:28 +0300

    refactoring

--- src/utills.py
+++ src/utills.py
@@ -2,38 +2,7 @@ import math
 import cv2
 import numpy as np
 from nptyping import Array
-from typing import List, Optional, Sequence, Union, Tuple
-
-from .obj_loader import OBJ
-
-
-class CounterFilter:
-    """
-    Save last markers state for a 'max_losses' number of frames
-
-    Parameters
-    ----------
-        max_losses, int:
-            Number of frames to preserve corners
-    """
-
-    def __init__(self, max_losses: int = 10) -> None:
-        self.max_losses = max_losses
-        self.current_losses = 0
-        self.last_corners: Optional[List[Tuple[int, int]]] = None
-
-    def init(self, last_corners: List[Tuple[int, int]]):
-        if len(last_corners) > 0:
-            self.last_corners = last_corners
-            self.current_losses = 0
-
-    def get(self) -> List[Tuple[int, int]]:
-        if self.current_losses < self.max_losses and self.last_corners is not None: 
-            self.current_losses += 1
-            return self.last_corners
-        else:
-            return []
-
+from typing import Sequence, Tuple
 
 
 def compute_corner(marker_shape: Tuple[int, int], homography: Array[float]) -> Array[Tuple[int, int]]:
@@ -83,39 +52,8 @@ def hex_to_rgb(hex_color: str) -> Sequence[int]:
     return tuple(int(hex_color[i:i + h_len // 3], 16) for i in range(0, h_len, h_len // 3))
 
 
-def render(img: Array[int],
-           obj: OBJ,
-           scale_factor: float,
-           projection: Array[float],
-           marker_shape: Tuple[int, int],
-           color: Union[bool, Sequence[int]] = False) -> Array[int]:
-    vertices = obj.vertices
-    scale_matrix = np.eye(3) * scale_factor
-    h, w = marker_shape
-    tmp_image = np.zeros_like(img)
-
-    for face in obj.faces:
-        face_vertices = face[0]
-        points = np.array([vertices[vertex - 1] for vertex in face_vertices])
-        points = np.dot(points, scale_matrix)
-        # render model in the middle of the reference surface. To do so,
-        # model points must be displaced
-        points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
-        # print(points)
-        dst = cv2.perspectiveTransform(points.reshape(-1, 1, 3), projection)
-        imgpts = np.int32(dst)
-        if color is False:
-            cv2.fillConvexPoly(tmp_image, imgpts, (137, 27, 211))
-        else:
-            color = hex_to_rgb(face[-1])
-            color = color[::-1] # reverse
-            cv2.fillConvexPoly(tmp_image, imgpts, color)
-    return np.uint8(np.where(tmp_image == 0, 1, 0.5) * img) + tmp_image//2
-
-
 def mask_from_contours(contours: Sequence[Tuple[int, int]], image: Array[int]) -> Array[int]:
     mask = np.zeros(image.shape[:2], dtype=np.uint8)
-    # mask = cv2.drawContours(mask, contours, -1, 255, cv2.FILLED)
     for cnt in contours:
         mask = cv2.fillPoly(mask, [cnt], 255)
     return mask.astype(np.uint8)
