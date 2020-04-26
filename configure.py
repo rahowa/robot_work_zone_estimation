1199d30f862aa4c45c5879fa94f3f42a946222c5
rahowa <roninfromsvf@gmail.com>
2020-04-26 21:39:58 +0300

    remove unnecessary imports

--- configure.py
+++ configure.py
@@ -1,20 +1,6 @@
-import os
-from src.render import RenderZone
 import sys
-import json
-from typing import Dict, Any, Optional, Tuple
-
-import cv2
-import numpy as np
-from nptyping import Array
-import streamlit as st
-from streamlit import sidebar as sb
-
 sys.path.append('./src')
-from src.obj_loader import OBJ
-from src.calibrate_camera import (CameraParams, save_camera_params,
-                                  load_camera_params, calibrate_camera)
-from src.aruco_zone_estimation import ArucoZoneEstimator, ARUCO_MARKER_SIZE
+
 from src.aruco_params import main_aruco
 
 
