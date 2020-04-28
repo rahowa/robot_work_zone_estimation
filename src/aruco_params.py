57c3eff091d951370c8e24c4aca0c876c66c0e36
rahowa <roninfromsvf@gmail.com>
2020-04-28 15:04:35 +0300

    add params for workzone size

--- src/aruco_params.py
+++ src/aruco_params.py
@@ -1,27 +1,47 @@
 import os
-from src.render import RenderZone
 import sys
-import json
-from typing import Dict, Any, Optional, Tuple
-
 import cv2
 import numpy as np
 import streamlit as st
+from typing import Optional, Tuple
 from streamlit import sidebar as sb
 
 sys.path.append('./src')
 from src.obj_loader import OBJ
+from src.render import RenderZone
 from src.calibrate_camera import (CameraParams, save_camera_params,
                                   load_camera_params, calibrate_camera)
 from src.aruco_zone_estimation import ArucoZoneEstimator, ARUCO_MARKER_SIZE
-from src.common_params import (save_config, get_cap,get_camera_params,
+from src.common_params import (save_config, get_cap, get_camera_params,
                                get_calibration_params, get_model_params)
+from src.workzone import Workzone
+
+
+def get_workzone_params() -> Tuple[int, int, int, int]:
+    sb.markdown("Params of robot workzone")
+    wz_center_x = sb.number_input("X coordinate of workzone center",
+                                  min_value=0,
+                                  max_value=500,
+                                  value=0)
+    wz_center_y = sb.number_input("Y coordinate of workzone center",
+                                  min_value=0,
+                                  max_value=500,
+                                  value=0)
+    wz_height = sb.number_input("Height of workzone",
+                                min_value=1,
+                                max_value=100,
+                                value=14)
+    wz_width = sb.number_input("Width of workzone",
+                               min_value=1,
+                               max_value=100,
+                               value=14)
+    return wz_center_x, wz_center_y, wz_height, wz_width
 
 
 def get_aruco_marker_params() -> Tuple[str, float]:
     sb.markdown("Params of marker")
     marker_size = sb.selectbox("Chose aruco marker size",
-                                ("4x4", "5x5", "6x6", "7X7"))
+                               ("4x4", "5x5", "6x6", "7X7"))
     marker_world_size = sb.number_input("Lenght of real world marker",
                                         min_value=0.0,
                                         max_value=100.0,
@@ -44,6 +64,8 @@ def main_aruco() -> None:
     frame_width, frame_height = get_camera_params()
     marker_size, marker_world_size = get_aruco_marker_params()
     scale_factor_model = get_model_params()
+    wz_cx, wz_cy, wz_height, wz_width = get_workzone_params()
+
     camera_params: Optional[CameraParams] = None
     show_calibration_params = sb.checkbox("Camera calibration")
     if show_calibration_params:
@@ -57,7 +79,7 @@ def main_aruco() -> None:
         if sb.button("Calibrate cemera"):
             st.write("Start camera calibration process")
             with st.spinner("Calibrating camera..."):
-                camera_params = calibrate_camera(path_to_images, 
+                camera_params = calibrate_camera(path_to_images,
                                                  frame_height,
                                                  frame_width,
                                                  board_height,
@@ -95,16 +117,19 @@ def main_aruco() -> None:
         cap.release()
         exit(0)
 
-    workzone_model = OBJ("src/data/3dmodels/Cylinder.obj", swapyz=True)
-    renderer = RenderZone(workzone_model, scale_factor_model, (7, 7))
-    estimator = ArucoZoneEstimator(marker_world_size, 
+    zone = Workzone(wz_cx, wz_cy, wz_height, wz_width)
+    estimator = ArucoZoneEstimator(marker_world_size,
                                    ARUCO_MARKER_SIZE[marker_size],
-                                   camera_params)
+                                   camera_params,
+                                   zone)
     while True:
         ret, scene = cap.read()
         if not ret:
             break
-        
-        scene = estimator.estimate(scene, None)
+
+        zone_polygon = estimator.estimate(scene)
+        zone_polygon = np.array(zone_polygon).reshape(-1, 1, 2)
+        zone_polygon = np.clip(zone_polygon, 0, np.inf)
+        scene = cv2.polylines(scene, [np.int32(zone_polygon)], True, (255, 0, 0), 2, cv2.LINE_AA)
         viewer.image(scene, channels='BGR')
     cap.release()
